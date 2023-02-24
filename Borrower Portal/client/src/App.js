5a4949314779c171877ea4ff9c6c4dcd9096c79c
PaxtonTerryDev <117777648+PaxtonTerryDev@users.noreply.github.com>
2023-02-24 09:10:28 -0700

    Initial Commit + SignUp + SignIn + Dashboard

---
+++ Borrower Portal/client/src/App.js
@@ -0,0 +1,10 @@
+import SignUp from "./components/scenes/SignUp";
+import { Routes, Route } from "react-router-dom";
+import SignIn from "./components/scenes/SignIn";
+import BorrowerDashboard from "./components/scenes/BorrowerDashboard";
+
+function App() {
+	return <BorrowerDashboard />;
+}
+
+export default App;
