95074b3dc6db486ff15a465fb78d83303ddce461
PaxtonTerry96 <PaxtonT@utahcounty.gov>
2023-02-27 13:12:55 -0700

    updated list items for respective tabs

--- Borrower Portal/client/src/components/reusable/ListItems.jsx
+++ Borrower Portal/client/src/components/reusable/ListItems.jsx
@@ -11,62 +11,76 @@ import LayersIcon from "@mui/icons-material/Layers";
 import AssignmentIcon from "@mui/icons-material/Assignment";
 
 export const mainListItems = (
-	<React.Fragment>
-		<ListItemButton>
-			<ListItemIcon>
-				<DashboardIcon />
-			</ListItemIcon>
-			<ListItemText primary="Dashboard" />
-		</ListItemButton>
-		<ListItemButton>
-			<ListItemIcon>
-				<ShoppingCartIcon />
-			</ListItemIcon>
-			<ListItemText primary="Orders" />
-		</ListItemButton>
-		<ListItemButton>
-			<ListItemIcon>
-				<PeopleIcon />
-			</ListItemIcon>
-			<ListItemText primary="Customers" />
-		</ListItemButton>
-		<ListItemButton>
-			<ListItemIcon>
-				<BarChartIcon />
-			</ListItemIcon>
-			<ListItemText primary="Reports" />
-		</ListItemButton>
-		<ListItemButton>
-			<ListItemIcon>
-				<LayersIcon />
-			</ListItemIcon>
-			<ListItemText primary="Integrations" />
-		</ListItemButton>
-	</React.Fragment>
+  <React.Fragment>
+    <ListItemButton>
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="My Loan" />
+    </ListItemButton>
+
+    <ListItemButton>
+      <ListItemIcon>
+        <ShoppingCartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Documents" />
+    </ListItemButton>
+
+    <ListItemButton>
+      <ListItemIcon>
+        <ShoppingCartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Signatures" />
+    </ListItemButton>
+
+    <ListItemButton>
+      <ListItemIcon>
+        <ShoppingCartIcon />
+      </ListItemIcon>
+      <ListItemText primary="My LO" />
+    </ListItemButton>
+  </React.Fragment>
 );
 
 export const secondaryListItems = (
-	<React.Fragment>
-		<ListSubheader component="div" inset>
-			Saved reports
-		</ListSubheader>
-		<ListItemButton>
-			<ListItemIcon>
-				<AssignmentIcon />
-			</ListItemIcon>
-			<ListItemText primary="Current month" />
-		</ListItemButton>
-		<ListItemButton>
-			<ListItemIcon>
-				<AssignmentIcon />
-			</ListItemIcon>
-			<ListItemText primary="Last quarter" />
-		</ListItemButton>
-		<ListItemButton>
-			<ListItemIcon>
-				<AssignmentIcon />
-			</ListItemIcon>
-			<ListItemText primary="Year-end sale" />
-		</ListItemButton>
-	</React.Fragment>
+  <React.Fragment>
+    <ListSubheader component="div" inset>
+      Application Details
+    </ListSubheader>
+
+    <ListItemButton>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Personal Information" />
+    </ListItemButton>
+
+    <ListItemButton>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Employment / Income" />
+    </ListItemButton>
+
+    <ListItemButton>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Assets / Liabilities" />
+    </ListItemButton>
+
+    <ListItemButton>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Property Owned" />
+    </ListItemButton>
+
+    <ListItemButton>
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Loan / Property Info" />
+    </ListItemButton>
+  </React.Fragment>
 );
